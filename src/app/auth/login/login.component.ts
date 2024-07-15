313823a5ebcfa65849e807f621301aa69aa6b56e
armanpoor <hossein.armanpoor@gmail.com>
2024-07-15 14:58:42 +0330

    3

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 
 @Component({
+  standalone: true,
   selector: 'app-login',
   templateUrl: './login.component.html',
+  imports: [ReactiveFormsModule],
 })
 export class LoginComponent {
   loginForm: FormGroup;
@@ -36,15 +43,15 @@ export class LoginComponent {
   onSubmit() {
     if (this.loginForm.valid) {
       const { username, password } = this.loginForm.value;
-      this.authService.login(username, password).subscribe(
-        (response) => {
+      this.authService.login(username, password).subscribe({
+        next: (response) => {
           this.router.navigate(['/']); // Navigate to the home page or another protected route
         },
-        (error) => {
+        error: (error) => {
           console.error('Login failed', error);
           // Handle login error
-        }
-      );
+        },
+      });
     }
   }
 }
--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,5 +1,11 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Router,
+} from '@angular/router';
+import { Observable } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
 @Injectable({
@@ -8,11 +14,23 @@ import { AuthService } from '../services/auth.service';
 export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) {}
 
-  canActivate(): boolean {
-    if (!this.authService.isAuthenticated) {
+  /**
+   * Determines if the user can activate a route by checking if they are authenticated.
+   * If authenticated, returns true. Otherwise, navigates to the login page and returns false.
+   *
+   * @param {ActivatedRouteSnapshot} next - The route to be activated.
+   * @param {RouterStateSnapshot} state - The current router state.
+   * @return {Observable<boolean> | Promise<boolean> | boolean} - True if authenticated, false otherwise.
+   */
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    if (this.authService.isAuthenticated()) {
+      return true;
+    } else {
       this.router.navigate(['/login']);
       return false;
     }
-    return true;
   }
 }
